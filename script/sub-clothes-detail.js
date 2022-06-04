50eb6e9de23288b0e070206da40f98f637e334f2
itsjuhee <itsjuheee@gmail.com>
2022-06-04 17:59:02 +0900

    quantity

--- script/sub-clothes-detail.js
+++ script/sub-clothes-detail.js
@@ -1,15 +1,25 @@
+// menu //
 
-    const
-    quantity = document.querySelector('.quantity > div > input'),
-    btn_plus = document.querySelector('.plus'),
-    btn_minus = document.querySelector('.minus');
+const elBtnMenu = document.querySelector('.m-btn-menu');
+const elMenu = document.querySelector('.m-menu');
+const elMenuClose = document.querySelector('.close');
 
-    let
-    num = 1;
-    
-    btn_plus.addEventListener('click', function(){
-        quantity.value = ++ num;
-    });
-    btn_minus.addEventListener('click', function(){
-        quantity.value = -- num;
-    });
+elBtnMenu.addEventListener('click', function(){elMenu.classList.add('active');});
+elMenuClose.addEventListener('click', function(){elMenu.classList.remove('active');});
+
+// quantity + - //
+
+const
+quantity = document.querySelector('.qua-res'),
+btn_plus = document.querySelector('.plus'),
+btn_minus = document.querySelector('.minus');
+
+let
+num = 1;
+
+btn_plus.addEventListener('click', function(){
+    quantity.textContent = ++ num;
+});
+btn_minus.addEventListener('click', function(){
+    quantity.textContent = -- num;
+});
